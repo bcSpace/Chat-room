41a141a3b94dac33893ee6f29a3c2eef1d5b344f
bc <bcspace72@gmail.com>
2018-01-29 13:01:26 -0500

    Client chat gui
    Includes a chat box and a line for sending messages
    Includes a button for sending messages and button for disconnecting.

--- src/client/gui/MainChat.java
+++ src/client/gui/MainChat.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -20,13 +21,15 @@ public class MainChat {
 	
 	private JFrame frame; 
 	
+	//chat
 	private JScrollPane scroll;
 	private JTextArea area; 
 	
+	//messaging and controls
 	private JPanel input;
 	private JTextField message; 
 	private JButton send; 
-	
+	private JButton disconnect; 
 	
 	public MainChat(Chatter chat) {
 		this.chat = chat;
@@ -44,14 +47,17 @@ public class MainChat {
 
 		area = new JTextArea(20,20);
 		area.setEditable(false);
+		area.setLineWrap(true);
 		scroll = new JScrollPane(area);
 		
 		input = new JPanel();
 		message =  new JTextField(15);
 		send = new JButton("Send");
+		disconnect = new JButton("Disconnect");
 		input.setLayout(new FlowLayout());
 		input.add(message);
 		input.add(send);
+		input.add(disconnect);
 		
 		frame.add(scroll, "Center");
 		frame.add(input, "South");
@@ -68,10 +74,27 @@ public class MainChat {
 			  } 
 		});
 		
+		disconnect.addActionListener(new ActionListener() { 
+			  public void actionPerformed(ActionEvent e) { 
+				  disconnect();
+			  } 
+		});
+		
+		
 	}
 	
 	public void start() {
 		frame.setVisible(true);
+	}
+	
+	public void disconnect() {
+		chat.disconnect();
+	}
+	
+	public void restart() {
+		area.setText("");
+		message.setText("");
+		frame.setVisible(false);
 	}
 	
 	private void sendMessage() {
@@ -85,6 +108,8 @@ public class MainChat {
 	
 	public void message(String s) {
 		area.append(s + "\n");
+		JScrollBar bar = scroll.getVerticalScrollBar();
+		bar.setValue(bar.getMaximum());
 	}
 
 }
